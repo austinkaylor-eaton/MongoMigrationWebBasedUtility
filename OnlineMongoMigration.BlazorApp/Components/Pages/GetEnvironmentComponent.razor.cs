aa635718fcdfea2871f36f3279fa877b8970b981
Austin Kaylor <austinjkaylor@eaton.com>
2025-06-30 15:09:21 -0400

    Adding a new .NET 8 blazor app for the mongo migration tool

---
+++ OnlineMongoMigration.BlazorApp/Components/Pages/GetEnvironmentComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineMongoMigration.BlazorApp.Components.Pages;
+
+public partial class GetEnvironmentComponent : ComponentBase
+{
+    [Inject]
+    private IWebHostEnvironment WebHostEnvironment { get; set; }
+
+    private string EnvironmentName => WebHostEnvironment.EnvironmentName;
+}
